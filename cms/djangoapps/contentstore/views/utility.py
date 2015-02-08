ed02e044cce5f64b4b2958c1eafe0b06bbc769ae
stv <stv@stanford.edu>
2015-02-08 15:27:52 -0800

    Fix library call for utilities page

--- cms/djangoapps/contentstore/views/utility.py
+++ cms/djangoapps/contentstore/views/utility.py
@@ -13,7 +13,7 @@ from contentstore.utils import reverse_course_url
 from xmodule.modulestore.django import modulestore
 from opaque_keys.edx.keys import CourseKey
 
-# from .access import has_course_access
+from ..course import get_course_and_check_access
 
 __all__ = ['utility_handler']
 
@@ -31,9 +31,8 @@ def utility_handler(request, course_key_string):
         json: return json representing all utilities.
     """
     course_key = CourseKey.from_string(course_key_string)
-    # if not has_course_access(request.user, course_key):
-    #    raise PermissionDenied()
-    raise PermissionDenied()
+    if not get_course_and_check_access(request.user, course_key):
+        raise PermissionDenied()
     course_module = modulestore().get_course(course_key)
     json_request = 'application/json' in request.META.get('HTTP_ACCEPT', 'application/json')
     if request.method == 'GET':
