8e45ab842088ef7926369f4b37a7fad2993de8f8
stv <stv@stanford.edu>
2015-02-20 21:07:56 -0800

    wip

--- lms/static/js/query-params.js
+++ lms/static/js/query-params.js
@@ -1,9 +1,19 @@
-// http://james.padolsey.com/javascript/bujs-1-getparameterbyname/
-function getParameterByName(name) {
-    var match = RegExp('[?&]' + name + '=([^&]*)')
-                    .exec(window.location.search);
+(function (window) {
+    var openedx;
+    var utilities;
+    function getParameterByName(name) {
+        // http://james.padolsey.com/javascript/bujs-1-getparameterbyname/
+        var match = RegExp('[?&]' + name + '=([^&]*)')
+                        .exec(window.location.search);
 
-    return match ?
-        decodeURIComponent(match[1].replace(/\+/g, ' '))
-        : null;
-}
+        return match ?
+            decodeURIComponent(match[1].replace(/\+/g, ' '))
+            : null;
+    }
+
+    window = window || {};
+    openedx = window.openedx || {};
+    utilities = openedx.utilities = openedx.utilities || {};
+    utilities.getParameterByName = getParameterByName;
+    window.openedx = openedx;
+}(window));
