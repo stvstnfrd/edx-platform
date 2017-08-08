65d135b4a1ad24c91f9dbe769bbb7c591e4cc4c0
Po Tsui <potsui@stanford.edu>
2017-08-08 14:25:26 -0700

    Make ExtraInfo into fields in Django Admin
    `Register_cme/extrainfo` in Django Admin was previously displaying users
    as `ExtraInfo` objects which admins had to click on individually to see
    each user's information. Each user is now displayed with fields:
    username, email, last and first name. Username is clickable to view more
    information.

--- openedx/stanford/djangoapps/register_cme/admin.py
+++ openedx/stanford/djangoapps/register_cme/admin.py
@@ -2,7 +2,27 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import ExtraInfo
 
-admin.site.register(ExtraInfo)
+
+class ExtraInfoAdmin(admin.ModelAdmin):
+    """
+    Admin interface for ExtraInfo model.
+    """
+    list_display = (
+        'user',
+        'get_email',
+        'last_name',
+        'first_name',
+    )
+
+    def get_email(self, obj):
+        return obj.user.email
+    get_email.short_description = 'Email address'
+
+    class Meta(object):
+        model = ExtraInfo
+
+admin.site.register(ExtraInfo, ExtraInfoAdmin)
