686c5bec7e785960915a65552f56adcaff6ee439
Po Tsui <potsui@stanford.edu>
2017-08-16 15:03:07 -0700

    Fix jshint errors and reorder declarations

--- cms/static/js/factories/bulkupdate.js
+++ cms/static/js/factories/bulkupdate.js
@@ -5,9 +5,13 @@ define([
     'use strict';
 
     return function (updateUrl, statusUrl) {
-        var resetBtn = $('.view-bulkupdate .reset-button'),
-            submitBtn = $('.view-bulkupdate .submit-button'),
-            defaults = [
+        var $applyMaxAttempts = $('#max-attempts .apply-existing-checkbox'),
+            $applyShowAnswer = $('#show-answer .apply-existing-checkbox'),
+            $maxAttempts = $('#max-attempts .setting-input'),
+            $showAnswer = $('#show-answer .setting-input'),
+            $resetBtn = $('.view-bulkupdate .reset-button'),
+            $submitBtn = $('.view-bulkupdate .submit-button'),
+            DEFAULT_ERROR_MESSAGES = [
                 gettext('There was an error during the submit process.') + '\n',
                 gettext('There was an error while validating the setting values you submitted.') + '\n',
                 gettext('There was an error while updating your problem settings in our database.') + '\n'
@@ -23,40 +27,19 @@ define([
                 'never'
             ],
             unloading = false,
-            previousUpdate = BulkUpdate.storedUpdate(),
-            $maxAttempts = $('#max-attempts .setting-input'),
-            $showAnswer = $('#show-answer .setting-input'),
-            $applyMaxAttempts = $('#max-attempts .apply-existing-checkbox'),
-            $applyShowAnswer = $('#show-answer .apply-existing-checkbox');
+            previousUpdate = BulkUpdate.storedUpdate();
 
         var onComplete = function () {
-            resetBtn.show();
+            $resetBtn.show();
         };
 
         var onReset = function () {
             BulkUpdate.reset();
-            submitBtn.show();
-            resetBtn.hide();
+            $submitBtn.show();
+            $resetBtn.hide();
         };
 
-        $(window).on('beforeunload', function (event) { unloading = true; });
-
-        // Display the status of last update on page load
-        if (previousUpdate) {
-            var maxAttempts = previousUpdate.update.maxAttempts;
-            var showAnswer = previousUpdate.update.showAnswer;
-
-            if (maxAttempts) {
-                $maxAttempts.val(previousUpdate.update.maxAttempts);
-                $applyMaxAttempts.prop('checked', true);
-            }
-            if (showAnswer) {
-                $showAnswer.val(previousUpdate.update.showAnswer);
-                $applyShowAnswer.prop('checked', true);
-            }
-
-            BulkUpdate.resume().then(onComplete);
-        }
+        $(window).on('beforeunload', function () { unloading = true; });
 
         var getSettingsData = function() {
             var data = {};
@@ -73,8 +56,46 @@ define([
             return data;
         };
 
-        var onSubmit = function(e) {
-            console.log("Pressed submit button");
+        var loadPreviousUpdate = function() {
+            if (previousUpdate) {
+                var maxAttempts = previousUpdate.update.maxAttempts;
+                var showAnswer = previousUpdate.update.showAnswer;
+
+                if (maxAttempts) {
+                    $maxAttempts.val(previousUpdate.update.maxAttempts);
+                    $applyMaxAttempts.prop('checked', true);
+                }
+                if (showAnswer) {
+                    $showAnswer.val(previousUpdate.update.showAnswer);
+                    $applyShowAnswer.prop('checked', true);
+                }
+
+                BulkUpdate.resume().then(onComplete);
+            }
+        };
+
+        var validateData = function(data) {
+            if (data.maxAttempts !== null || data.showAnswer !== null) {
+                var msg;
+                var maxAttemptsIsValid = data.maxAttempts === null || data.maxAttempts >= 0;
+                var showAnswerIsValid = data.showAnswer === null || SHOW_ANSWER_OPTIONS.indexOf(data.showAnswer) >= 0;
+
+                if (!maxAttemptsIsValid) {
+                    msg = gettext('Not a valid value for MaxAttempts. Please enter a different value.');
+                    $('.error-block').html(msg).show();
+                    console.error(msg);
+                }
+                if (!showAnswerIsValid) {
+                    msg = gettext('Not a valid value for ShowAnswer. Please enter a different value.');
+                    $('.error-block').html(msg).show();
+                    console.error(msg);
+                }
+                return maxAttemptsIsValid && showAnswerIsValid;
+            }
+            return false;
+        };
+
+        var onSubmit = function() {
             var data = getSettingsData();
             if (validateData(data)) {
                 BulkUpdate.start(
@@ -84,10 +105,10 @@ define([
                         'fillerMaxAttempts/fillerShowAnswer',
                         data.maxAttempts + '/' + data.showAnswer
                     )
-                ).then(onComplete);
+                );
 
                 BulkUpdate.pollStatus();
-                submitBtn.hide();
+                $submitBtn.hide();
 
                 $.ajax({
                     type: "POST",
@@ -104,7 +125,7 @@ define([
                             errMsg = serverMsg.ErrMsg;
                             if (serverMsg.hasOwnProperty('Stage')) {
                                 stage = Math.abs(serverMsg.Stage);
-                                BulkUpdate.cancel(defaults[stage] + errMsg, stage);
+                                BulkUpdate.cancel(DEFAULT_ERROR_MESSAGES[stage] + errMsg, stage);
                             }
                         } catch (e) {
                             errMsg = '';
@@ -126,40 +147,22 @@ define([
             return false;
         };
 
-        var validateData = function(data) {
-            if (data.maxAttempts != null || data.showAnswer != null) {
-                var maxAttemptsIsValid = data.maxAttempts == null || data.maxAttempts >= 0;
-                var showAnswerIsValid = data.showAnswer == null || SHOW_ANSWER_OPTIONS.indexOf(data.showAnswer) >= 0;
-
-                if (!maxAttemptsIsValid) {
-                    var msg = gettext('Not a valid value for MaxAttempts. Please enter a different value.')
-                    $('.error-block').html(msg).show();
-                    console.error(msg);
-                }
-                if (!showAnswerIsValid) {
-                    var msg = gettext('Not a valid value for ShowAnswer. Please enter a different value.')
-                    $('.error-block').html(msg).show();
-                    console.error(msg);
-                }
-                return maxAttemptsIsValid && showAnswerIsValid;
-            }
-            return false;
-        };
-
         domReady(function () {
-            resetBtn.bind('click', function (e) {
+            loadPreviousUpdate();
+
+            $resetBtn.bind('click', function (e) {
                 e.preventDefault();
                 onReset();
             });
-            submitBtn.bind('click', function (e) {
+            $submitBtn.bind('click', function (e) {
                 e.preventDefault();
                 onSubmit(e);
             });
 
             if (previousUpdate) {
-                resetBtn.show();
+                $resetBtn.show();
             } else {
-                submitBtn.show();
+                $submitBtn.show();
             }
         });
     };
