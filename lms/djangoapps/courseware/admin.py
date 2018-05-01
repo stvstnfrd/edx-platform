19343f7926b44305744dbf4cefc27f9c0bd4da64
stv <stv@stanford.edu>
2018-05-01 14:50:47 -0700

    Take ours: lms/djangoapps/courseware/admin.py

--- lms/djangoapps/courseware/admin.py
+++ lms/djangoapps/courseware/admin.py
@@ -2,13 +2,9 @@
 django admin pages for courseware model
 '''
 
-<<<<<<< HEAD
-from courseware.models import CoursePreference
-from courseware.models import StudentModule, OfflineComputedGrade, OfflineComputedGradeLog
-=======
->>>>>>> f9fa460a74446b533b356e754848af6f56c141a1
 from ratelimitbackend import admin
 
+from courseware.models import CoursePreference
 from courseware.models import OfflineComputedGrade, OfflineComputedGradeLog, StudentModule
 
 admin.site.register(StudentModule)
