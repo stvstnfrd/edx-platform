34387fa86c3bf173fba47ce565692d2e7b6bf4ff
Giulio Gratta <giuliog@stanford.edu>
2019-07-31 14:17:10 -0700

    Reword account deletion messages
    - Rename instances of `certificates` to `Statements of Accomplishment`
    - Remove instances of `edX` and replace with `Lagunita` where appropriate

--- lms/static/js/student_account/components/StudentAccountDeletion.jsx
+++ lms/static/js/student_account/components/StudentAccountDeletion.jsx
@@ -39,9 +39,9 @@ export class StudentAccountDeletion extends React.Component {
   render() {
     const { deletionModalOpen, socialAuthConnected, isActive } = this.state;
     const loseAccessText = StringUtils.interpolate(
-      gettext('You may also lose access to verified certificates and other program credentials like MicroMasters certificates. If you want to make a copy of these for your records before proceeding with deletion, follow the instructions for {htmlStart}printing or downloading a certificate{htmlEnd}.'),
+      gettext('You may also lose access to Statements of Accomplishment. If you want to make a copy of these for your records before proceeding with deletion, {htmlStart}print or download your Statements of Accomplishment{htmlEnd}.'),
       {
-        htmlStart: '<a href="https://edx.readthedocs.io/projects/edx-guide-for-students/en/latest/SFD_certificates.html#printing-a-certificate" target="_blank">',
+        htmlStart: '<a href="/dashboard" target="_blank">',
         htmlEnd: '</a>',
       },
     );
@@ -83,8 +83,8 @@ export class StudentAccountDeletion extends React.Component {
     return (
       <div className="account-deletion-details">
         <p className="account-settings-header-subtitle">{ gettext('We’re sorry to see you go!') }</p>
-        <p className="account-settings-header-subtitle">{ gettext('Please note: Deletion of your account and personal data is permanent and cannot be undone. EdX will not be able to recover your account or the data that is deleted.') }</p>
-        <p className="account-settings-header-subtitle">{ gettext('Once your account is deleted, you cannot use it to take courses on the edX app, edx.org, or any other site hosted by edX. This includes access to edx.org from your employer’s or university’s system and access to private sites offered by MIT Open Learning, Wharton Executive Education, and Harvard Medical School.') }</p>
+        <p className="account-settings-header-subtitle">{ gettext('Please note: Deletion of your account and personal data is permanent and cannot be undone. We will not be able to recover your account or the data that is deleted.') }</p>
+        <p className="account-settings-header-subtitle">{ gettext('Once your account is deleted, you will be unable to use this account to take any more courses on Lagunita.') }</p>
         <p
           className="account-settings-header-subtitle"
           dangerouslySetInnerHTML={{ __html: loseAccessText }}
--- lms/static/js/student_account/components/StudentAccountDeletionModal.jsx
+++ lms/static/js/student_account/components/StudentAccountDeletionModal.jsx
@@ -94,9 +94,9 @@ class StudentAccountDeletionConfirmationModal extends React.Component {
     } = this.state;
     const { onClose } = this.props;
     const loseAccessText = StringUtils.interpolate(
-      gettext('You may also lose access to verified certificates and other program credentials like MicroMasters certificates. If you want to make a copy of these for your records before proceeding with deletion, follow the instructions for {htmlStart}printing or downloading a certificate{htmlEnd}.'),
+      gettext('You may also lose access to Statements of Accomplishment. If you want to make a copy of these for your records before proceeding with deletion, {htmlStart}print or download your Statements of Accomplishment{htmlEnd}.'),
       {
-        htmlStart: '<a href="https://edx.readthedocs.io/projects/edx-guide-for-students/en/latest/SFD_certificates.html#printing-a-certificate" target="_blank">',
+        htmlStart: '<a href="/dashboard" target="_blank">',
         htmlEnd: '</a>',
       },
     );
@@ -137,8 +137,8 @@ class StudentAccountDeletionConfirmationModal extends React.Component {
                       <Icon id="delete-confirmation-body-warning-icon" className={['fa', 'fa-exclamation-triangle']} />
                     </div>
                     <div className="alert-content">
-                      <h3 className="alert-title">{ gettext('You have selected “Delete my account.” Deletion of your account and personal data is permanent and cannot be undone. EdX will not be able to recover your account or the data that is deleted.') }</h3>
-                      <p>{ gettext('If you proceed, you will be unable to use this account to take courses on the edX app, edx.org, or any other site hosted by edX. This includes access to edx.org from your employer’s or university’s system and access to private sites offered by MIT Open Learning, Wharton Executive Education, and Harvard Medical School.') }</p>
+                      <h3 className="alert-title">{ gettext('You have selected “Delete my account.” Deletion of your account and personal data is permanent and cannot be undone. We will not be able to recover your account or the data that is deleted.') }</h3>
+                      <p>{ gettext('If you proceed, you will be unable to use this account to take any more courses on Lagunita.') }</p>
                       <p dangerouslySetInnerHTML={{ __html: loseAccessText }} />
                     </div>
                   </div>
