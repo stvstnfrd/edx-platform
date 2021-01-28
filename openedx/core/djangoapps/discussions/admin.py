e28303deca7ead12ebdd01207b41827c488b5b0e
stvn <stvn@mit.edu>
2021-01-28 10:19:32 -0800

    Fix django-admin handling

--- openedx/core/djangoapps/discussions/admin.py
+++ openedx/core/djangoapps/discussions/admin.py
@@ -2,6 +2,7 @@
 Customize the django admin experience
 """
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
 from simple_history.admin import SimpleHistoryAdmin
 
 from openedx.core.djangoapps.config_model_utils.admin import StackedConfigModelAdmin
@@ -22,6 +23,46 @@ class DiscussionsConfigurationAdmin(SimpleHistoryAdmin):
     )
 
 
+class AllowListFilter(SimpleListFilter):
+    title = 'Allow List'
+    parameter_name = 'allow'
+
+    def lookups(self, request, model_admin):
+        queryset = model_admin.get_queryset(request)
+        values = tuple(
+            (
+                ','.join(filters[self.parameter_name] or ['None']),
+                ', '.join(filters[self.parameter_name] or ['None']),
+            )
+            for filters in queryset.values(self.parameter_name).distinct()
+        )
+        return values
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        filter_kwargs = {}
+        if value:
+            if ',' not in value:
+                if value == 'None':
+                    filter_kwargs[self.parameter_name + '__exact'] = ''
+                    return queryset.filter(**filter_kwargs)
+                else:
+                    filter_kwargs[self.parameter_name + '__contains'] = value
+                    return queryset.filter(**filter_kwargs)
+            else:
+                for v in value.split(','):
+                    filter_kwargs[self.parameter_name + '__contains'] = v
+                    queryset = queryset.filter(**filter_kwargs)
+                return queryset
+        else:
+            return queryset
+
+
+class DenyListFilter(AllowListFilter):
+    title = 'Deny List'
+    parameter_name = 'deny'
+
+
 # class ProviderFilterAdmin(admin.ModelAdmin):
 class ProviderFilterAdmin(StackedConfigModelAdmin):
     search_fields = (
@@ -30,8 +71,8 @@ class ProviderFilterAdmin(StackedConfigModelAdmin):
     )
     list_filter = (
         'enabled',
-        'allow',
-        'deny',
+        AllowListFilter,
+        DenyListFilter,
     )
 
 
